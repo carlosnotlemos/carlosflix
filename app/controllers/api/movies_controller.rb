72657bf856094db226a15fc30a0b38728f6a1e9f
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-06-21 07:16:18 -0300

    feat(movie): ajustando para uma simples visualização

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,8 +1,34 @@
 class Api::MoviesController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    movies = Movie.all
+    render json: movies
+  end
+  def show
+    movie = Movie.find_by!(slug: params[:slug])
+    render json: movie
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Filme não encontrado" }, status: :not_found
+  end
+  def new; end
   def edit; end
   def update; end
-  def create; end
+
+  def create
+    ActiveRecord::Base.transaction do
+      @movie = Movie.new(movie_params)
+      if @movie.save
+        render json: @movie, status: :created
+      else
+        render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
+
   def destroy; end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :video_master_url, :description, :thumbnail_url)
+  end
 end
